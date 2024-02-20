ad231328540b5e3684094ec2d3fbcaa3f0f6cb33
Skqlixur <jmjmbo5@gmail.com>
2024-02-21 00:34:46 +0800

    update

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
-import enquirer from 'enquirer'
-import clipboard from 'clipboardy'
-const { prompt } = enquirer
-
-;(async () => {
-  const response = await prompt({
-    type: 'input',
-    name: 'expression',
-    message: 'Expression to evaluate:',
+const yargs = require('yargs')
+const args = yargs
+  .usage('Usage: node index.js [options] <expression>')
+  .option('f', {
+    alias: 'forloop',
+    describe: 'Evaluate the expression in a for loop',
+    type: 'boolean',
   })
+  .option('start', {
+    alias: 's',
+    describe: 'Start value for the for loop',
+    type: 'number',
+  })
+  .option('end', {
+    alias: 'e',
+    describe: 'End value for the for loop',
+    type: 'number',
+  })
+  .option('increment', {
+    alias: 'i',
+    describe: 'Step value for the for loop',
+    type: 'number',
+  })
+  .option('c', {
+    alias: 'copy',
+    describe: 'Copy the result to the clipboard',
+    type: 'boolean',
+  })
+  .help('h')
+  .alias('h', 'help').argv
 
-  const evaluatedResponse = eval(response.expression).toString()
+// Check if the -f or --forloop option is present
+if (args.f || args.forloop) {
+  quickEvalLoop()
+} else {
+  quickEval()
+}
 
-  await clipboard.write(evaluatedResponse)
-})()
+function quickEval() {
+  const result = eval(args._[0])
+  if (args.c || args.copy) {
+    const ncp = require('copy-paste')
+    ncp.copy(result, () => {})
+  } else {
+    console.log(result)
+  }
+}
+
+function quickEvalLoop() {
+  const start = args.start || 1
+  const end = args.end || 10
+  const step = args.step || 1
+  let result = ''
+
+  for (let i = start; i <= end; i += step) {
+    result += eval(args._[0].replace(/\$/g, i)) + '\n'
+  }
+
+  if (args.c || args.copy) {
+    const ncp = require('copy-paste')
+    ncp.copy(result, () => {})
+  } else console.log(result)
+}
